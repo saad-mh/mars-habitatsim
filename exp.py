68cd2d85d576528870f64f256ec7f2cb7ee3c04e
saad-mh <saad.ans2018@gmail.com>
2026-07-28 15:09:35 +0530

    Close stdin, stdout, and stderr for subprocesses in experiment execution

--- exp.py
+++ exp.py
@@ -460,6 +460,9 @@ for i, (name, args) in enumerate(EXPERIMENTS, start=1):
             sys.stdout.write(line)
             log.write(line)
 
+        proc.stdin.close()
+        proc.stdout.close()
+        proc.stderr.close()
         proc.wait()
 
     elapsed = time.time() - start
